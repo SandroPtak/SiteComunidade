f6c8ad223c7a3067b36ef6318ee46a447162e509
SandroPtak <105433119+SandroPtak@users.noreply.github.com>
2024-10-14 10:17:05 -0300

    estilizandoos formulários

--- main.py
+++ main.py
@@ -1,23 +1,34 @@
 from flask import Flask, render_template, url_for
+from forms import FormCriarConta, FormLogin
+
 app = Flask(__name__)
 
 lista_usuarios = ['Sandro', 'Ray', 'Marcelo', 'Hide', 'Lidia']
+
+app.config['SECRET_KEY'] = '3247f73eee75b941aa3889f747042b69'
+
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
+
 @app.route('/contato')
 def contato():
     return render_template('contato.html')
 
+
 @app.route('/usuarios')
 def usuarios():
-    return render_template('usuarios.html', lista_usuarios = lista_usuarios)
+    return render_template('usuarios.html', lista_usuarios=lista_usuarios)
+
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    form_login = FormLogin()
+    form_criarconta = FormCriarConta()
+    return render_template('login.html', form_login=form_login, form_criarconta=form_criarconta)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
