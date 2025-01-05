2a5e0925c0f1651dbecb2e1497085a7f5277cf39
SandroPtak <105433119+SandroPtak@users.noreply.github.com>
2025-01-05 10:42:04 -0300

    adicionado edição de posts

--- comunidade/forms.py
+++ comunidade/forms.py
@@ -40,4 +40,4 @@ class FormEditarPerfil(FlaskForm):
 class FormCriarPost(FlaskForm):
     titulo = StringField('Título do Post', validators=[DataRequired(), Length(2, 140)])
     corpo = TextAreaField('Escreva seu Post aqui', validators=[DataRequired()])
-    botao_submit_criarpost = SubmitField('Criar Post')
+    botao_submit_criarpost = SubmitField('Postar')
